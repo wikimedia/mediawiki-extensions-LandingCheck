ff06e7926bbd3120805e781b000bf7c50f622be3
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-10-29 19:35:15 +0000

    some working code

---
+++ LandingCheck.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for GeoLite extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Ryan Kaldari
+ */
+$messages['en'] = array(
+	'landingcheck-desc' => 'This extension facilitates the use of geotargeted localized landing pages. It is a replacement for GeoLite.',
+	'landingcheck' => 'LandingCheck',
+);
--- SpecialLandingCheck.php
+++ SpecialLandingCheck.php
@@ -4,14 +4,33 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-class LandingCheck extends SpecialPage {
+class SpecialLandingCheck extends SpecialPage {
 	
 	function __construct() {
 		// Register special page
 		parent::__construct( 'LandingCheck' );
 	}
+	
+	function execute( $sub ) {
+		global $wgOut, $wgUser, $wgRequest, $wgLandingPageBase;
+		
+		if ( $wgRequest->getVal( 'language' ) ) {
+			$language = ( preg_match( '/^[A-Za-z-]+$/', $wgRequest->getVal( 'language' ) ) );
+		} else {
+			$language = 'en';
+		}
+		$country = $wgRequest->getVal( 'country' );
+		$landingPage = $wgRequest->getVal( 'landing_page' );
+		
+		$tracking = '?' . wfArrayToCGI( array( 
+			'utm_source' => $wgRequest->getVal( 'utm_source' ),
+			'utm_medium' => $wgRequest->getVal( 'utm_medium' ),
+			'utm_campaign' => $wgRequest->getVal( 'utm_campaign' ),
+			'referrer' => $wgRequest->getHeader( 'referer' )
+		) );
+		
+		if ( 1 ) {
+			$wgOut->redirect( $wgLandingPageBase . '/' . $language . '/' . $country . $tracking );
+		}
+	}
 }
-
-//$wgOut->redirect( $this->getTitle( 'view' )->getLocalUrl( "template=$template" ) );
-
-?>
