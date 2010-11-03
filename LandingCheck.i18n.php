aa531af78236a1cddd67dfee3c649dddc086b5ed
Raimond Spekking <raymond@users.mediawiki.org>
2010-11-03 20:28:35 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-03 20:05:00 UTC)

--- LandingCheck.i18n.php
+++ LandingCheck.i18n.php
@@ -16,3 +16,27 @@ $messages['en'] = array(
 	'landingcheck' => 'LandingCheck',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'landingcheck-desc' => 'Ermöglicht eine erleichterte Nutzung von mit geografischen Daten verknüpften Zielwebsites in jeweils verfügbaren Sprachen',
+	'landingcheck' => 'Websitezielprüfung',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'landingcheck-desc' => 'Facilita le uso de paginas de arrivata specific al position geographic e lingua del usator',
+	'landingcheck' => 'Arrivata localisate',
+);
+
+/** Slovenian (Slovenščina)
+ * @author Dbc334
+ */
+$messages['sl'] = array(
+	'landingcheck-desc' => 'Omogoča uporabo jezikovno prilagojenih ciljnih strani glede na geolokacijo',
+	'landingcheck' => 'PreverjanjePristajališča',
+);
+
