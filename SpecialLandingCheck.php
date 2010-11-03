c6ff67ff1b136afbd0a5ea616dcad91536606596
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-11-03 02:00:24 +0000

    some comments and clean-up

--- SpecialLandingCheck.php
+++ SpecialLandingCheck.php
@@ -5,9 +5,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 /**
- * This page checks to see if a version of a landing page exists for the user's language and 
- * country. If not, it looks for a version localized for the user's language. If that doesn't exist 
- * either, it looks for the English version. If any of those exist, it then redirects the user.
+ * This checks to see if a version of a landing page exists for the user's language and country. 
+ * If not, it looks for a version localized for the user's language. If that doesn't exist either, 
+ * it looks for the English version. If any of those exist, it then redirects the user.
  */
 class SpecialLandingCheck extends SpecialPage {
 	
@@ -17,12 +17,14 @@ class SpecialLandingCheck extends SpecialPage {
 	}
 	
 	public function execute( $sub ) {
-		global $wgOut, $wgUser, $wgRequest, $wgScript;
+		global $wgOut, $wgRequest;
 		
+		// Pull in query string parameters
 		$language = $wgRequest->getVal( 'language', 'en' );
 		$country = $wgRequest->getVal( 'country', 'US' );
 		$landingPage = $wgRequest->getVal( 'landing_page', 'Donate' );
 		
+		// Construct new query string for tracking
 		$tracking = wfArrayToCGI( array( 
 			'utm_source' => $wgRequest->getVal( 'utm_source' ),
 			'utm_medium' => $wgRequest->getVal( 'utm_medium' ),
@@ -30,21 +32,22 @@ class SpecialLandingCheck extends SpecialPage {
 			'referrer' => $wgRequest->getHeader( 'referer' )
 		) );
 		
-		if ( strval( $landingPage ) !== '' ) {
-			$targetTexts = array(
-				$landingPage . '/' . $language . '/' . $country,
-				$landingPage . '/' . $language
-			);
-			if ( $language != 'en' ) {
-				$targetTexts[] = $landingPage . '/en';
-			}
-			foreach ( $targetTexts as $targetText ) {
-				$target = Title::newFromText( $targetText );
-				if ( $target && $target->isKnown() && $target->getNamespace() == NS_MAIN ) {
-					$wgOut->redirect( $target->getLocalURL( $tracking ) );
-					return;
-				} 
-			}
+		// Build array of landing pages to check for
+		$targetTexts = array(
+			$landingPage . '/' . $language . '/' . $country,
+			$landingPage . '/' . $language
+		);
+		if ( $language != 'en' ) {
+			$targetTexts[] = $landingPage . '/en'; // English fallback
+		}
+		
+		// Go through the possible landing pages and redirect the user as soon as one is found to exist
+		foreach ( $targetTexts as $targetText ) {
+			$target = Title::newFromText( $targetText );
+			if ( $target && $target->isKnown() && $target->getNamespace() == NS_MAIN ) {
+				$wgOut->redirect( $target->getLocalURL( $tracking ) );
+				return;
+			} 
 		}
 		
 	}
