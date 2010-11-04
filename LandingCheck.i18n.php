acb9abd8489a640cacbd84fe80c1467784cfc5f5
Raimond Spekking <raymond@users.mediawiki.org>
2010-11-04 21:19:12 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-04 21:05:00 UTC)

--- LandingCheck.i18n.php
+++ LandingCheck.i18n.php
@@ -24,6 +24,14 @@ $messages['de'] = array(
 	'landingcheck' => 'Websitezielprüfung',
 );
 
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'landingcheck-desc' => 'Macht e lychteri Nutzig megli vu mit geografische Date verchnipfte Ziilwebsyte',
+	'landingcheck' => 'Ziilwebsytepriefig',
+);
+
 /** Interlingua (Interlingua)
  * @author McDutchie
  */
@@ -32,11 +40,58 @@ $messages['ia'] = array(
 	'landingcheck' => 'Arrivata localisate',
 );
 
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'landingcheck-desc' => 'Овозможува употреба на локализирани целни страници во зависност од земјата на корисникот',
+	'landingcheck' => 'Местоположбена проверка за целната страница',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'landingcheck-desc' => "Maakt het mogelijk om via geolocatie gelokaliseerde landingspagina's aan te bieden",
+	'landingcheck' => 'Landingscontrole',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'landingcheck-desc' => 'Ułatwia automatyczny wybór strony ze względu na lokalizację geograficzną',
+	'landingcheck' => 'LandingCheck',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'landingcheck-desc' => "A facìlita l'usagi ëd pàgine d'ateragi localisà geo-mirà",
+	'landingcheck' => 'LandingCheck',
+);
+
+/** Portuguese (Português)
+ * @author GoEThe
+ */
+$messages['pt'] = array(
+	'landingcheck-desc' => 'Facilita o uso de páginas de destino localizadas geodireccionadas',
+);
+
 /** Slovenian (Slovenščina)
  * @author Dbc334
  */
 $messages['sl'] = array(
 	'landingcheck-desc' => 'Omogoča uporabo jezikovno prilagojenih ciljnih strani glede na geolokacijo',
 	'landingcheck' => 'PreverjanjePristajališča',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'landingcheck-desc' => 'Cung cấp khả năng trình bày các trang đích đến bản địa hóa',
+	'landingcheck' => 'LandingCheck',
 );
 
