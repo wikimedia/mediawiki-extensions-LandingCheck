25a67ca94a6a4cfb8e1a8b6a153e771c2234f54d
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-12-22 21:44:49 +0000

    adding country_code as tracking parameter

--- SpecialLandingCheck.php
+++ SpecialLandingCheck.php
@@ -42,7 +42,7 @@ class SpecialLandingCheck extends SpecialPage {
 			'utm_source' => $wgRequest->getVal( 'utm_source' ),
 			'utm_medium' => $wgRequest->getVal( 'utm_medium' ),
 			'utm_campaign' => $wgRequest->getVal( 'utm_campaign' ),
-			'country' => $country,
+			'country_code' => $country,
 			'referrer' => $wgRequest->getHeader( 'referer' )
 		) );
 		
