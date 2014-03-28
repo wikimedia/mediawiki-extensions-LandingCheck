05c938c0ba700e727a562480df03eb11c4ce2e97
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-28 08:32:23 +0100

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ie014292a0a65f57a3bbe7125201c4e3ed11d517f

--- LandingCheck.php
+++ LandingCheck.php
@@ -13,15 +13,16 @@ EOT;
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'LandingCheck',
-	'version' => '2.0',
+	'version' => '2.1.0',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:LandingCheck',
 	'author' => array( 'Ryan Kaldari', 'Arthur Richards' ),
 	'descriptionmsg' => 'landingcheck-desc',
 );
 
-$dir = dirname( __FILE__ ) . '/';
+$dir = __DIR__ . '/';
 
 $wgAutoloadClasses['SpecialLandingCheck'] = $dir . 'SpecialLandingCheck.php';
+$wgMessagesDirs['LandingCheck'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['LandingCheck'] = $dir . 'LandingCheck.i18n.php';
 $wgExtensionMessagesFiles['LandingCheckAlias'] = $dir . 'LandingCheck.alias.php';
 $wgSpecialPages['LandingCheck'] = 'SpecialLandingCheck';
@@ -33,14 +34,14 @@ $wgPriorityCountries = array();
 
 /**
  * It is possible to configure a separate server running LandingCheck to handle
- * requests for priority countries and another for non-priority countries. By 
+ * requests for priority countries and another for non-priority countries. By
  * default, the local instance of LandingCheck will handle both unless the following
  * variables are configured.
- * 
+ *
  * The URLs contained in these variables should be the full URL to the location
  * of LandingCheck - the query string will be appended. For example:
  *   $wgLandingCheckPriorityURLBase = '//wikimediafoundation.org/wiki/Special:LandingCheck';
- * 
+ *
  * LandingCheck will compare the host portion of these URLs to what is
  * configured for $wgServer. If the hosts match, the LandingCheck will just
  * handle the request locally. If these are not set, LandingCheck will default to
