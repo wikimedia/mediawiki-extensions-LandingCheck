22f657051352fab02e7436649ec34616658230e3
Reedy <reedy@wikimedia.org>
2016-01-13 00:44:41 +0000

    Revert "No alias file in extension"
    This reverts commit e48ca545159d506d2768f4de12ed99f088738fb1.
    
    Change-Id: Ic9a7ef8c74ac3d139016232266ad63723a839a6c

--- LandingCheck.php
+++ LandingCheck.php
@@ -4,6 +4,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'LandingCheck' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['LandingCheck'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['LandingCheckAlias'] = __DIR__ . 'LandingCheck.alias.php';
 	/*wfWarn(
 		'Deprecated PHP entry point used for LandingCheck extension. ' .
 		'Please use wfLoadExtension instead, see ' .
