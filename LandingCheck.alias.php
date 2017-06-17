5ca9f3aca32689e5caeb5fd47227efd9e9680149
Umherirrender <umherirrender_de.wp@web.de>
2017-06-18 00:03:07 +0200

    Use short array syntax in alias file
    Change-Id: I85b39204cb3a8cceb48e3833b5e9591d8a9d06c8

--- LandingCheck.alias.php
+++ LandingCheck.alias.php
@@ -5,96 +5,95 @@
  * @file
  * @ingroup Extensions
  */
-// @codingStandardsIgnoreFile
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'LandingCheck' => array( 'LandingCheck' ),
-);
+$specialPageAliases['en'] = [
+	'LandingCheck' => [ 'LandingCheck' ],
+];
 
 /** Arabic (العربية) */
-$specialPageAliases['ar'] = array(
-	'LandingCheck' => array( 'تحقق_الهدف' ),
-);
+$specialPageAliases['ar'] = [
+	'LandingCheck' => [ 'تحقق_الهدف' ],
+];
 
 /** Egyptian Arabic (مصرى) */
-$specialPageAliases['arz'] = array(
-	'LandingCheck' => array( 'تحقق_الهدف' ),
-);
+$specialPageAliases['arz'] = [
+	'LandingCheck' => [ 'تحقق_الهدف' ],
+];
 
 /** Western Balochi (بلوچی رخشانی) */
-$specialPageAliases['bgn'] = array(
-	'LandingCheck' => array( 'ایربوتین_ئی_بررسی' ),
-);
+$specialPageAliases['bgn'] = [
+	'LandingCheck' => [ 'ایربوتین_ئی_بررسی' ],
+];
 
 /** German (Deutsch) */
-$specialPageAliases['de'] = array(
-	'LandingCheck' => array( 'Websitezielprüfung' ),
-);
+$specialPageAliases['de'] = [
+	'LandingCheck' => [ 'Websitezielprüfung' ],
+];
 
 /** Zazaki (Zazaki) */
-$specialPageAliases['diq'] = array(
-	'LandingCheck' => array( 'CérBerz' ),
-);
+$specialPageAliases['diq'] = [
+	'LandingCheck' => [ 'CérBerz' ],
+];
 
 /** Persian (فارسی) */
-$specialPageAliases['fa'] = array(
-	'LandingCheck' => array( 'بررسی_فرود' ),
-);
+$specialPageAliases['fa'] = [
+	'LandingCheck' => [ 'بررسی_فرود' ],
+];
 
 /** Haitian (Kreyòl ayisyen) */
-$specialPageAliases['ht'] = array(
-	'LandingCheck' => array( 'VerifikasyonAteri' ),
-);
+$specialPageAliases['ht'] = [
+	'LandingCheck' => [ 'VerifikasyonAteri' ],
+];
 
 /** Italian (italiano) */
-$specialPageAliases['it'] = array(
-	'LandingCheck' => array( 'VerificaDestinazione' ),
-);
+$specialPageAliases['it'] = [
+	'LandingCheck' => [ 'VerificaDestinazione' ],
+];
 
 /** Korean (한국어) */
-$specialPageAliases['ko'] = array(
-	'LandingCheck' => array( '방문확인' ),
-);
+$specialPageAliases['ko'] = [
+	'LandingCheck' => [ '방문확인' ],
+];
 
 /** Macedonian (македонски) */
-$specialPageAliases['mk'] = array(
-	'LandingCheck' => array( 'ПроверкаНаОдредница' ),
-);
+$specialPageAliases['mk'] = [
+	'LandingCheck' => [ 'ПроверкаНаОдредница' ],
+];
 
 /** Norwegian Bokmål (norsk bokmål) */
-$specialPageAliases['nb'] = array(
-	'LandingCheck' => array( 'Landingssjekk' ),
-);
+$specialPageAliases['nb'] = [
+	'LandingCheck' => [ 'Landingssjekk' ],
+];
 
 /** Low Saxon (Netherlands) (Nedersaksies) */
-$specialPageAliases['nds-nl'] = array(
-	'LandingCheck' => array( 'Laandingsziedkontraole' ),
-);
+$specialPageAliases['nds-nl'] = [
+	'LandingCheck' => [ 'Laandingsziedkontraole' ],
+];
 
 /** Dutch (Nederlands) */
-$specialPageAliases['nl'] = array(
-	'LandingCheck' => array( 'Landingspaginacontrole' ),
-);
+$specialPageAliases['nl'] = [
+	'LandingCheck' => [ 'Landingspaginacontrole' ],
+];
 
 /** Sicilian (sicilianu) */
-$specialPageAliases['scn'] = array(
-	'LandingCheck' => array( 'VerificaDestinazione' ),
-);
+$specialPageAliases['scn'] = [
+	'LandingCheck' => [ 'VerificaDestinazione' ],
+];
 
 /** Cantonese (粵語) */
-$specialPageAliases['yue'] = array(
-	'LandingCheck' => array( '登陸檢查' ),
-);
+$specialPageAliases['yue'] = [
+	'LandingCheck' => [ '登陸檢查' ],
+];
 
 /** Simplified Chinese (中文（简体）‎) */
-$specialPageAliases['zh-hans'] = array(
-	'LandingCheck' => array( '着陆页检查' ),
-);
+$specialPageAliases['zh-hans'] = [
+	'LandingCheck' => [ '着陆页检查' ],
+];
 
 /** Traditional Chinese (中文（繁體）‎) */
-$specialPageAliases['zh-hant'] = array(
-	'LandingCheck' => array( '到達檢查' ),
-);
+$specialPageAliases['zh-hant'] = [
+	'LandingCheck' => [ '到達檢查' ],
+];
