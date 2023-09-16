9977828e2e4c4ce1ef88384807192ed03fae4a37
Umherirrender <umherirrender_de.wp@web.de>
2023-09-16 19:06:37 +0200

    Use HookHandlers for core hook
    Bug: T346490
    Change-Id: Ia3d65315f99654632009334c38d4d0bca2f39d07

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,7 +2,11 @@
 
 namespace MediaWiki\Extension\LandingCheck;
 
-class Hooks {
+// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+
+use MediaWiki\Languages\Hook\Language__getMessagesFileNameHook;
+
+class Hooks implements Language__getMessagesFileNameHook {
 
 	/**
 	 * Register es-419 as a language supported by this extension but not by
@@ -12,7 +16,7 @@ class Hooks {
 	 * @param string $code language code
 	 * @param string &$file path of Messages file as found by MediaWiki core
 	 */
-	public static function onGetMessagesFileName( $code, &$file ) {
+	public function onLanguage__getMessagesFileName( $code, &$file ) {
 		if ( $code === 'es-419' ) {
 			$file = dirname( __DIR__ ) . '/messages/MessagesEs_419.php';
 		}
